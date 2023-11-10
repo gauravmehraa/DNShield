c0b89aaf5d5c6deca6e1bcfff1808e245f4c00f0
gauravmehraa <gauravmehra158@gmail.com>
2023-11-10 18:51:33 +0530

    Display outgoing packet data

--- main.py
+++ main.py
@@ -3,38 +3,32 @@ import dnslib
 import binascii
 
 def main():
-    with pydivert.WinDivert("udp.DstPort == 53 or udp.SrcPort == 53") as w: # 53 = DNS port
+    with pydivert.WinDivert("udp.DstPort == 53") as window: # 53 = DNS port
         print("DNS interceptor started...")
-        for packet in w:
+        for packet in window:
             try:
                 p = binascii.unhexlify(packet.payload.hex())
                 record = dnslib.DNSRecord.parse(p)
-                print(record)
-
-
-                '''
                 record = str(dnslib.DNSRecord.parse(packet.payload)).split()
-                query = {}
-                record = record[2:]
-                while ";;" in record:
-                    record.remove(";;")
                 
+                query = {}
                 record = [field.replace(";","").replace(":","").replace(",","").rstrip(".") for field in record]
-                record[16] = record[16] + " " + record[17]
-                del record[17]
 
-                for i in range(0, len(record)-1, 2):
-                    query[record[i]] = record[i+1]
+                query["opcode"] = record[record.index("opcode")+1]
+                query["status"] = record[record.index("status")+1]
+                query["id"] = record[record.index("id")+1]
+                query["domain"] = record[(record.index("SECTION")+1)]
+                query["class"] = record[(record.index("SECTION")+2)]
+                query["type"] = record[(record.index("SECTION")+3)]
                 
                 for k, v in query.items():
-                    print(f"{k}: {v}")'''
+                    print(f"{k}: {v}")
                     
-
-                print("-" * 50)
+                print("-" * 100)
             except Exception as e:
                 print("Error processing DNS query:", str(e))
             finally:
-                w.send(packet, recalculate_checksum=True)
+                window.send(packet, recalculate_checksum=True)
 
 if __name__ == "__main__":
     main()
